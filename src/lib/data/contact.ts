ce5ae517f4b64692d52a59c491c39fd9d0c956dc
AzanoRivers <job.devcode@gmail.com>
2026-05-13 16:00:17 -0500

    horario add

--- src/lib/data/contact.ts
+++ src/lib/data/contact.ts
@@ -6,7 +6,7 @@ export const contactInfo: ContactInfo = {
   mobile: '304 289 6066',
   whatsapp: '3014897223',
   email: 'clientes@grafivision.com.co',
-  schedule: 'Lun.\u2013Vie.: 7:30\u201317:30 \u00b7 S\u00e1b.: 8:30\u201312:00',
+  schedule: 'Lunes a Viernes: 7:30\u201317:30 \u00b7 S\u00e1bado: 8:30\u201312:00',
   mapsEmbedUrl: import.meta.env.PUBLIC_MAPS_EMBED_URL ?? '',
   social: {
     instagram: 'https://www.instagram.com/grafivision_editores',
